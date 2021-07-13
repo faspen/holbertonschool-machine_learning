30b0c68f66ebb84ba6dc8833a755e64f94554f8e
faspen <2335@holbertonschool.com>
2021-07-13 19:46:52 +0000

    11-12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Bracin the elements"""
+
+
+def np_elementwise(mat1, mat2):
+    """Add, sub, mult, div"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
