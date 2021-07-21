4991391574b9e6c9fa3c12857b3a1e454b56fecd
faspen <2335@holbertonschool.com>
2021-07-21 22:10:33 +0000

    literally was not returning lol

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -13,6 +13,10 @@ def poly_integral(poly, C=0):
         return new
     for i in range(len(poly)):
         if isinstance(poly[i], int) or isinstance(poly[i], float):
-            new.append(poly[i] / (i + 1))
+            num = poly[i] / (i + 1)
+            if float.is_integer(num):
+                num = int(num)
+            new.append(num)
         else:
             return None
+    return new
