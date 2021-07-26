ba17ad0464e637b8a6b9ee9179ceab8923546447
faspen <2335@holbertonschool.com>
2021-07-26 16:13:44 +0000

    pmf

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -17,3 +17,21 @@ class Poisson():
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculate pmf"""
+        k = int(k)
+
+        if k < 0:
+            return 0
+        e = 2.7182818284590452353602874713527
+        num = 1
+
+        x = e ** (-self.lambtha)
+        y = self.lambtha ** k
+
+        for i in range(1, k + 1):
+            num = num * i
+        pmf = (x * y) / num
+
+        return pmf
