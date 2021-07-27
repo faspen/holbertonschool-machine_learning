ae2319769321de38680338c25348d893ac3bc14d
faspen <2335@holbertonschool.com>
2021-07-27 14:56:30 +0000

    normal complete

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -20,3 +20,26 @@ class Normal():
             self.mean = float(sum(data) / len(data))
             step = [(x - self.mean) ** 2 for x in data]
             self.stddev = pow((sum(step) / len(data)), 0.5)
+
+    def z_score(self, x):
+        """Get z score"""
+        return ((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """Get x value"""
+        return ((z * self.stddev) + self.mean)
+
+    def pdf(self, x):
+        """Get PDF"""
+        i = (1 / (self.stddev * ((2 * 3.14159265)**(1 / 2))))
+        j = (-1 / 2) * ((x - self.mean) / self.stddev)**2
+        return i * (2.71828**j)
+
+    def cdf(self, x):
+        """Get CDF"""
+        i = (x - self.mean) / (self.stddev * (2 ** (1 / 2)))
+        erf_1 = (2 / 3.14159265 ** (1 / 2))
+        erf_2 = (i - ((i**3) / 3) + ((i**5) / 10) -
+                 ((i**7) / 42) + ((i**9) / 216))
+        val = (1 / 2) * (1 + erf_1 * erf_2)
+        return val
