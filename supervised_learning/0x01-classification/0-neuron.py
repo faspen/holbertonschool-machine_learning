85e5a1e526226d9e90fe3e840d0528d372716fb9
faspen <2335@holbertonschool.com>
2021-08-02 17:09:45 +0000

    task 0

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+
+
+import numpy as np
+
+
+class Neuron():
+    """class for neuron"""
+
+    def __init__(self, nx):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
