33b1ce1b51c66c7628df2c89f0d140ef2934c728
faspen <2335@holbertonschool.com>
2021-08-18 18:28:11 +0000

    FINALLY

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -8,21 +8,11 @@ import tensorflow as tf
 def create_batch_norm_layer(prev, n, activation):
     """Return tensor of output"""
     kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    base = tf.layers.Dense(units=n, name='layer', kernel_initializer=kernel)
+    base = tf.layers.Dense(units=n, kernel_initializer=kernel)
     A = base(prev)
-    mean, var = tf.nn.moments(A, [0])
-    beta = tf.Variable(tf.constant(0.0, shape=[n]),
-                       trainable=True)
-    gamma = tf.Variable(tf.constant(1.0, shape=[n]),
-                        trainable=True)
+    mean, var = tf.nn.moments(A, axes=[0])
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
 
-    batch = tf.nn.batch_normalization(
-        A,
-        mean,
-        var,
-        offset=beta,
-        scale=gamma,
-        variance_epsilon=1e-8)
-    if activation is True:
-        return activation(batch)
-    return batch
+    batch = tf.nn.batch_normalization(A, mean, var, beta, gamma, 1e-8)
+    return activation(batch)
