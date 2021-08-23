5dd74523c97e434f4f22282d423b6134cbea8a27
faspen <2335@holbertonschool.com>
2021-08-23 15:45:05 +0000

    1-sensitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""Sensitivity module"""
 
 
+import numpy as np
+
+
+def sensitivity(confusion):
+    """Return array shape (classes,)"""
+    pos = np.sum(confusion, axis=1)
+    true = np.diagonal(confusion)
+    res = true / pos
+    return res
