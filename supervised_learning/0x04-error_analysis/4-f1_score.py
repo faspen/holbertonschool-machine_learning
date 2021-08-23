1bda45360e46b29f0fdb69f7223a0a9a8c4c6a54
faspen <2335@holbertonschool.com>
2021-08-23 16:04:25 +0000

    4-f1_score.py

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""F1 score"""
 
 
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """Return f1 score"""
+    prec = sensitivity(confusion)
+    recall = precision(confusion)
+    return (2 * (prec * recall) / (prec + recall))
