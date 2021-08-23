7f14295d87c33b2819301b747d348ceb3d364ed2
faspen <2335@holbertonschool.com>
2021-08-23 15:49:21 +0000

    2-precision.py

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""Precision module"""
 
 
+import numpy as np
+
+
+def precision(confusion):
+    """Return array shape (classes,)"""
+    relevant = np.sum(confusion, axis=0)
+    retrieved = np.diagonal(confusion)
+    res = retrieved / relevant
+    return res
