29f339220cc5834b81d30848c5b678e0588f7418
faspen <2335@holbertonschool.com>
2021-08-23 15:55:25 +0000

    3-specificity.py

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""Specificity module"""
 
 
+import numpy as np
+
+
+def specificity(confusion):
+    """Return array shape (classes,)"""
+    fp = confusion.sum(axis=0) - np.diag(confusion)
+    fn = confusion.sum(axis=1) - np.diag(confusion)
+    tp = np.diag(confusion)
+    tn = confusion.sum() - (fp + fn + tp)
+    res = tn / (tn + fp)
+    return res
