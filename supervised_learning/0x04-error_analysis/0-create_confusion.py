4e597a747eb3f66d72a3dd9582a543baefbb6d45
faspen <2335@holbertonschool.com>
2021-08-23 15:34:55 +0000

    0-create_confusion.py

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""Create confusion"""
 
 
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """Return numpy array (classes, classes)"""
+    return np.matmul(labels.T, logits)
