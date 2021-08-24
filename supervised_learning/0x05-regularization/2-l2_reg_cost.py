591b3cd50b98a977a18adeb2d8e1f3cd929bd15d
faspen <2335@holbertonschool.com>
2021-08-24 16:03:34 +0000

    2-l2_reg_cost.py

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""L2 Regularization Cost"""
 
 
+import tensorflow as tf
 
+
+def l2_reg_cost(cost):
+    """L2 Regularization tensorflow style"""
+    return cost + tf.losses.get_regularization_losses()
