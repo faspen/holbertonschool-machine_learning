5707ecf7c2a8a3354824213c7f23964fc20977f4
faspen <2335@holbertonschool.com>
2021-08-24 16:11:24 +0000

    3-l2_reg_create_layer.py

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+"""L2 Regularization layer"""
 
 
+import tensorflow as tf
 
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Return output of the new layer"""
+    function = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    regular = tf.contrib.layers.l2_regularizer(lambtha)
+
+    layer = tf.layers.Dense(
+        units=n,
+        name='layer',
+        activation=activation,
+        kernel_initializer=function,
+        kernel_regularizer=regular)
+    return layer(prev)
