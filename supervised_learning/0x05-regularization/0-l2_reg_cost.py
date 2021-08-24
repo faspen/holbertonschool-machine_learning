04375a6658b16aec8bcbfaebd683a45cf8bf50cc
faspen <2335@holbertonschool.com>
2021-08-24 15:48:24 +0000

    0-l2_reg_cost.py

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""L2 Regularization Cost"""
 
 
+import numpy as np
 
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """Return cost of network for l2"""
+    norm = 0
+
+    for k, weight in weights.items():
+        if k[0] == 'W':
+            norm += np.linalg.norm(weight)
+    cost += lambtha / (2 * m) * norm
+    return cost
