c497be1e9f7b7ba49cf6f7636e1e69875a0ad17f
faspen <2335@holbertonschool.com>
2021-08-25 15:36:43 +0000

    100-slice_like_a_ninja.py

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Slice like a ninja"""
+
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """Return new array"""
+    pieces = []
+    for i in range(max(axes) + 1):
+        if i in axes.keys():
+            pieces.append(slice(*axes.get(i)))
+        else:
+            pieces.append(slice(None, None, None))
+    return matrix[tuple(pieces)]
