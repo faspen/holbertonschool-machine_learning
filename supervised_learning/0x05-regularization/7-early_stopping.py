9548b1bdc87514b229fb5dbd980e289136ffd5fc
faspen <2335@holbertonschool.com>
2021-08-25 15:30:27 +0000

    7-early_stopping.py

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""Early stopping"""
 
 
-
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Determine if gradient descent should stop"""
+    if (opt_cost - cost) > threshold:
+        count = 0
+    else:
+        count += 1
+    if count == patience:
+        boolean = True
+    else:
+        boolean = False
+    return boolean, count
