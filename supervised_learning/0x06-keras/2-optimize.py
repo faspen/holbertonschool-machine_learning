0cb28a83832a530f2684cc4b4ebe984f0fc803be
faspen <2335@holbertonschool.com>
2021-08-30 15:32:56 +0000

    2-optimize.py

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-""" """
+"""Optimize keras"""
 
 
 import tensorflow.keras as K
 
 
+def optimize_model(network, alpha, beta1, beta2):
+    """Return None"""
+    opt = K.optimizers.Adam(alpha, beta_1=beta1, beta_2=beta2)
 
+    network.compile(
+        optimizer=opt,
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+
+    return None
