bc7790b8203952795aaf1fe9f7fae1254116e61b
faspen <2335@holbertonschool.com>
2021-08-30 17:58:14 +0000

    9-model.py

--- supervised_learning/0x06-keras/9-model.py
+++ supervised_learning/0x06-keras/9-model.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-""" """
+"""Train the model"""
 
 
 import tensorflow.keras as K
 
 
+def save_model(network, filename):
+    """Save the model"""
+    network.save(filename)
+    return None
 
+
+def load_model(filename):
+    """Load the model"""
+    return K.models.load_model(filename)
