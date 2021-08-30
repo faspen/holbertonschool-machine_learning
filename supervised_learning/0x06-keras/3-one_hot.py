0f0d6a39693205a697d4c8ad0cd794857fde1209
faspen <2335@holbertonschool.com>
2021-08-30 15:38:27 +0000

    3-one_hot.py

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""One hot"""
 
 
 import tensorflow.keras as K
 
 
-
+def one_hot(labels, classes=None):
+    """Return a one-hot matrix"""
+    return K.utils.to_categorical(labels, num_classes=classes)
