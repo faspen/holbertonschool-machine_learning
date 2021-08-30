0a6439b16f5896adf3278599d6ffeca68f1fa8f0
faspen <2335@holbertonschool.com>
2021-08-30 15:44:04 +0000

    4-train.py

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
-""" """
+"""Train the model"""
 
 
 import tensorflow.keras as K
 
 
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        verbose=True,
+        shuffle=False):
+    """Return the history"""
+    history = network.fit(
+        x=data,
+        y=labels,
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=verbose,
+        shuffle=shuffle)
 
+    return history
