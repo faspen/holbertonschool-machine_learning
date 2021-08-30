970e869060ab15452c5472dd5ff7797160b29147
faspen <2335@holbertonschool.com>
2021-08-30 17:04:48 +0000

    6-train.py

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
-""" """
+"""Train the model"""
 
 
 import tensorflow.keras as K
 
 
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """Return the history"""
+    time = []
+    if validation_data:
+        early_stopping = K.callbacks.EarlyStopping(
+            monitor='val_loss', patience=patience)
+        time.append(early_stopping)
+    history = network.fit(
+        x=data,
+        y=labels,
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=verbose,
+        shuffle=shuffle, validation_data=validation_data,
+        callbacks=time)
 
+    return history
