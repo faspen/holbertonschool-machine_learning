463a642535e6fdf59b760047b76f9ecba36da4df
faspen <2335@holbertonschool.com>
2021-08-31 15:37:32 +0000

    10-weights.py

--- supervised_learning/0x06-keras/10-weights.py
+++ supervised_learning/0x06-keras/10-weights.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-""" """
+"""Save and Load Weights"""
 
 
 import tensorflow.keras as K
 
 
+def save_weights(network, filename, save_format='h5'):
+    """Save weights and return none"""
+    network.save_weights(filepath=filename, save_format=save_format)
+    return None
 
+
+def load_weights(network, filename):
+    """Load the weights and return none"""
+    network.load_weights(filename)
+    return None
