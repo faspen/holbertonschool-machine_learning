89b3255830cdb0359bd7bed5f8f6e36cb1770a00
faspen <2335@holbertonschool.com>
2021-09-07 15:02:28 +0000

    0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
-""" """
+"""Convolutional forward prop"""
 
 
 import numpy as np
 
 
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """Forward prop over conv layer of nn"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
 
+    if padding is 'same':
+        ph = (((sh * h_prev) - sh + kh - h_prev) // 2)
+        pw = (((sw * w_prev) - sw + kw - w_prev) // 2)
+    if padding is 'valid':
+        ph, pw = 0, 0
+
+    A_prev = np.pad(A_prev, [(0, 0), (ph, ph), (pw, pw),
+                    (0, 0)], 'constant', constant_values=0)
+    zh = ((h_prev + (2 * ph) - kh) // sh) + 1
+    zw = ((w_prev + (2 * pw) - kw) // sw) + 1
+    conv = np.zeros((m, zh, zw, c_new))
+    i = np.arange(0, m)
+
+    for h in range(zh):
+        for w in range(zw):
+            for z in range(c_new):
+                conv[i, h, w, z] = activation(np.sum(np.multiply(
+                    A_prev[i, h * sh:kh + h * sh, w * sw:kw + w * sw],
+                    W[:, :, :, z]), axis=(1, 2, 3))) + b[0, 0, 0, z]
+
+    return conv
