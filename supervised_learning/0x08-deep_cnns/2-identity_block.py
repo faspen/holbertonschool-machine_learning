ee9871073d4b6d044284ac1d15650e5e160cca3b
faspen <2335@holbertonschool.com>
2021-09-13 16:46:22 +0000

    2-identity_block.py

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -5,4 +5,32 @@
 import tensorflow.keras as K
 
 
+def identity_block(A_prev, filters):
+    """Return identity block"""
+    F11, F3, F12 = filters
 
+    init = K.initializers.he_normal(seed=None)
+
+    conv_1 = K.layers.Conv2D(filters=F11, kernel_size=1,
+                             padding='same',
+                             kernel_initializer=init)(A_prev)
+
+    batch_norm_1 = K.layers.BatchNormalization()(conv_1)
+    activation_1 = K.layers.Activation('relu')(batch_norm_1)
+
+    conv_2 = K.layers.Conv2D(filters=F3, kernel_size=3,
+                             padding='same',
+                             kernel_initializer=init)(activation_1)
+
+    batch_norm_2 = K.layers.BatchNormalization()(conv_2)
+    activation_2 = K.layers.Activation('relu')(batch_norm_2)
+
+    conv_3 = K.layers.Conv2D(filters=F12, kernel_size=1,
+                             padding='same',
+                             kernel_initializer=init)(activation_2)
+
+    batch_norm_3 = K.layers.BatchNormalization()(conv_3)
+    add_layer = K.layers.Add()([batch_norm_3, A_prev])
+    activation_3 = K.layers.Activation('relu')(add_layer)
+
+    return activation_3
