1a9513023e125b2535718d2930ffe77bc94de86f
faspen <2335@holbertonschool.com>
2021-09-14 15:51:05 +0000

    6-transition_layer.py

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -5,4 +5,18 @@
 import tensorflow.keras as K
 
 
+def transition_layer(X, nb_filters, compression):
+    """Output of trans layer, number of filters"""
+    init = K.initializers.he_normal()
+    fil = int(nb_filters * compression)
 
+    batch_norm = K.layers.BatchNormalization()(X)
+    act = K.layers.Activation('relu')(batch_norm)
+    conv = K.layers.Conv2D(filters=fil, kernel_size=1,
+                           padding='same', strides=1,
+                           kernel_initializer=init)(act)
+
+    avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2),
+                                         padding='same')(conv)
+
+    return avg_pool, fil
