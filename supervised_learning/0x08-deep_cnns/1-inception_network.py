1ba84a339f1328518f83f9ba424a1fda15d2529a
faspen <2335@holbertonschool.com>
2021-09-16 15:29:22 +0000

    i dunno

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -9,14 +9,14 @@ inception_block = __import__('0-inception_block').inception_block
 def inception_network():
     """Keras model for network"""
     init = K.initializers.he_normal()
-    inputs = K.Input(shape=(224, 224, 3))
+    x = K.Input(shape=(224, 224, 3))
 
     conv_1 = K.layers.Conv2D(filters=64, kernel_size=(7, 7),
                              padding='same', activation='relu',
-                             strides=(2, 2), kernel_initializer=init)(inputs)
+                             strides=(2, 2), kernel_initializer=init)(x)
 
     max_pool_1 = K.layers.MaxPool2D(pool_size=(3, 3),
-                                       strides=(2, 2), padding='same')(conv_1)
+                                    strides=(2, 2), padding='same')(conv_1)
 
     conv_2 = K.layers.Conv2D(filters=64, kernel_size=(1, 1), padding='same',
                              activation='relu', strides=(1, 1),
@@ -26,13 +26,13 @@ def inception_network():
                              strides=(1, 1), kernel_initializer=init)(conv_2)
 
     max_pool_2 = K.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2),
-                                       padding='same')(conv_3)
+                                    padding='same')(conv_3)
 
     conc_1 = inception_block(max_pool_2, [64, 96, 128, 16, 32, 32])
     conc_2 = inception_block(conc_1, [128, 128, 192, 32, 96, 64])
 
     max_pool_3 = K.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2),
-                                       padding='same')(conc_2)
+                                    padding='same')(conc_2)
 
     conc_3 = inception_block(max_pool_3, [192, 96, 208, 16, 48, 64])
     conc_4 = inception_block(conc_3, [160, 112, 224, 24, 64, 64])
@@ -41,7 +41,7 @@ def inception_network():
     conc_7 = inception_block(conc_6, [256, 160, 320, 32, 128, 128])
 
     max_pool_4 = K.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2),
-                                       padding='same')(conc_7)
+                                    padding='same')(conc_7)
 
     conc_8 = inception_block(max_pool_4, [256, 160, 320, 32, 128, 128])
     conc_9 = inception_block(conc_8, [384, 192, 384, 48, 128, 128])
@@ -53,6 +53,6 @@ def inception_network():
     dense = K.layers.Dense(1000, activation='softmax',
                            kernel_initializer=init)(drop)
 
-    model = K.Model(inputs=inputs, outputs=dense)
+    model = K.Model(inputs=x, outputs=dense)
 
     return model
