155badf2a327f02c281f6931deb91b2cab5aadfd
faspen <2335@holbertonschool.com>
2021-09-20 16:12:13 +0000

    upload

---
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Transfer knowledge"""
+
+
+import tensorflow.keras as K
+
+
+def preprocess_data(X, Y):
+    """Pre-process cifar10 data"""
+
+
+if __name__ == "__main__":
+    
