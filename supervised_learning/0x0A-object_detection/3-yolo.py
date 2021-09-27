47eb0db64ff4d61e97de4b07546ff5de9992efea
faspen <2335@holbertonschool.com>
2021-09-27 15:49:25 +0000

    upload

---
+++ supervised_learning/0x0A-object_detection/3-yolo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" """
+
+
+import tensorflow.keras as K
+import numpy as np
+
+
+
---
+++ supervised_learning/0x0A-object_detection/5-yolo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" """
+
+
+import tensorflow.keras as K
+import numpy as np
+import cv2
+
+
+
