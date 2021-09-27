8b06e3bb46fb92d9e790324a689cd5c6ae9a4ba0
faspen <2335@holbertonschool.com>
2021-09-27 15:33:22 +0000

    comments and renaming

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -13,6 +13,7 @@ def preprocess_data(X, Y):
 
 
 if __name__ == "__main__":
+    # Ready the data
     (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
     X_train, Y_train = preprocess_data(X_train, Y_train)
     X_test, Y_test = preprocess_data(X_test, Y_test)
@@ -20,23 +21,32 @@ if __name__ == "__main__":
     i = K.Input(shape=(32, 32, 3))
     hf = (224 // 32)
     wf = (224 // 32)
+
+    # start with lambda layer
     densenet_model = K.layers.Lambda(
         lambda x: K.backend.resize_images(x, height_factor=hf,
                                           width_factor=wf,
                                           data_format='channels_last'))(i)
 
+    # Use DenseNet169 and initialize weights with imagenet
     DN169 = K.applications.DenseNet169(include_top=False,
                                        weights='imagenet',
                                        input_shape=(224, 224, 3))
-    pre_freeze = DN169(densenet_model, training=False)
-    pre_freeze = K.layers.GlobalAveragePooling2D(pre_freeze)
-    pre_freeze = K.layers.Dense(500, activation='relu')(pre_freeze)
-    pre_freeze = K.layers.Dropout(0.2)(pre_freeze)
-    output = K.layers.Dense(10, activation='softmax')(pre_freeze)
+
+    # first instance of freezing
+    dNet = DN169(densenet_model, training=False)
+    # Go through layer of pooling
+    dNet = K.layers.GlobalAveragePooling2D(dNet)
+    dNet = K.layers.Dense(500, activation='relu')(dNet)
+    dNet = K.layers.Dropout(0.2)(dNet)
+    # One more dense layer, 10 filters for the labels
+    output = K.layers.Dense(10, activation='softmax')(dNet)
 
     model = K.Model(inputs=i, outputs=output)
+    # Freeze
     DN169.trainable = False
 
+    # Finalize
     model.compile(loss='categorical_crossentropy',
                   optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
@@ -44,4 +54,5 @@ if __name__ == "__main__":
                      validation_data=(X_test, Y_test),
                      batch_size=300,
                      epochs=5, verbose=True)
+    # save it
     model.save('cifar10.h5')
