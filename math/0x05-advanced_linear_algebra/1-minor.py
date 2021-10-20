29a3ed3de2f6014a7723a1d891d502a6e485df1a
faspen <2335@holbertonschool.com>
2021-10-20 16:13:18 +0000

    implement

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,28 +2,6 @@
 """Minor calculator"""
 
 
-def determinant_helper(matrix, total=0):
-    """Assist in the calculation of determinant"""
-    indices = list(range(len(matrix)))
-
-    if len(matrix) == 2:
-        return (matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1])
-
-    for fc in indices:
-        tmp = matrix.copy()
-        tmp = matrix[1:]
-        rlen = len(tmp)
-
-        for r in range(rlen):
-            tmp[r] = tmp[r][0:fc] + tmp[r][fc + 1:]
-
-        sign = (-1) ** (fc % 2)
-        sub_det = determinant_helper(tmp)
-        total += sign * matrix[0][fc] * sub_det
-
-    return total
-
-
 def determinant(matrix):
     """Return determinant of matrix"""
     if matrix == [[]]:
@@ -41,8 +19,21 @@ def determinant(matrix):
 
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return (matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1])
 
-    return determinant_helper(matrix)
+    row = matrix[0]
+    total = 0
+    cof = 1
+    for i in range(len(matrix[0])):
+        tmp = [m[:] for m in matrix]
+        del tmp[0]
+        for val in tmp:
+            del val[i]
+        total += row[i] * determinant(tmp) * cof
+        cof *= -1
+
+    return total
 
 
 def minor_helper(matrix, i, j):
