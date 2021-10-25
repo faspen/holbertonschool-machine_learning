79ed0982b021d066a80d40288b44e1e45689cba2
faspen <2335@holbertonschool.com>
2021-10-25 15:43:52 +0000

    mean and correlation upload

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -7,4 +7,15 @@ import numpy as np
 
 def mean_cov(X):
     """Calculate mean and covariance of data set"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n = X.shape[0]
 
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0).reshape(1, X.shape[1])
+    X -= mean
+    cov = ((np.dot(X.T, X)) / (n - 1))
+
+    return mean, cov
--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -7,4 +7,12 @@ import numpy as np
 
 def correlation(C):
     """Calculate correlation matrix"""
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or (C.shape[0] != C.shape[1]):
+        raise ValueError("C must be a 2D square matrix")
 
+    v = np.sqrt(np.diag(C))
+    outer = np.outer(v, v)
+    corr = C / outer
+    return corr
