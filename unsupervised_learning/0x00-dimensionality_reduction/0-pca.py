3e8d62f5b5ec9b38751e8929b10f61a1aa7a0d2d
faspen <2335@holbertonschool.com>
2021-10-26 14:33:22 +0000

    upload

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""PCA module for task 0"""
+
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Perform PCA on dataset"""
+
---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""PCA module for task 1"""
+
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """Perform PCA on dataset"""
+
