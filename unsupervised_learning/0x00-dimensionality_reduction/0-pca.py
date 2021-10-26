186c78531ad19a8dd835dfb3de0392e25086d835
faspen <2335@holbertonschool.com>
2021-10-26 15:07:32 +0000

    mandatory tasks

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -7,4 +7,9 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Perform PCA on dataset"""
-
+    s, v, d = np.linalg.svd(X)
+    ratios = list(i / np.sum(v) for i in v)
+    variant = np.cumsum(ratios)
+    res = np.argwhere(variant >= var)[0, 0]
+    W = d.T[:, :(res + 1)]
+    return (W)
--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -7,4 +7,11 @@ import numpy as np
 
 def pca(X, ndim):
     """Perform PCA on dataset"""
+    X_mean = X - X.mean(axis=0)
+    s, v, d = np.linalg.svd(X_mean)
 
+    W = d.T
+    Wm = W[:, 0:ndim]
+    T = np.matmul(X_mean, Wm)
+
+    return T
