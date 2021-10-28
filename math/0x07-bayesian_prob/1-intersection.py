c81d0d3e187a150d6a0206aee5969aca8affa108
faspen <2335@holbertonschool.com>
2021-10-28 18:37:47 +0000

    adjustments to TypeError's

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -16,7 +16,7 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) or len(Pr.shape) != 1:
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     for counter in range(P.shape[0]):
         if P[counter] > 1 or P[counter] < 0:
