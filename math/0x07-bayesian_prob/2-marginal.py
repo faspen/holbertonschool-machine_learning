7654aa1c4a4400a4f828000cd8ea2f4151107e3f
faspen <2335@holbertonschool.com>
2021-10-28 14:39:49 +0000

    2-marginal.py

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
-"""Marginal Probability"""
+"""Marginal Probability module"""
 
 
 import numpy as np
 
 
 def marginal(x, n, P, Pr):
-    """Calculate the marginal probability of given data"""
+    """Calculate MP of obtaining certain data"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or len(Pr.shape) != 1:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    for counter in range(P.shape[0]):
+        if P[counter] > 1 or P[counter] < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+        if Pr[counter] > 1 or Pr[counter] < 0:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+    if np.isclose([np.sum(Pr)], [1]) == [False]:
+        raise ValueError("Pr must sum to 1")
 
+    fact = np.math.factorial
+    fact_coef = fact(n) / (fact(n - x) * fact(x))
+    likelihood = fact_coef * (P ** x) * ((1 - P) ** (n - x))
+
+    intersection = likelihood * Pr
+    marginal = np.sum(intersection)
+
+    return marginal
