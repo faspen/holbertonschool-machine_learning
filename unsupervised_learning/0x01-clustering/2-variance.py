9a7ec0d3e4546d11e5048aee0f4665c23c04895c
faspen <2335@holbertonschool.com>
2021-11-01 16:38:24 +0000

    2-variance.py

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -7,4 +7,19 @@ import numpy as np
 
 def variance(X, C):
     """Calculate total intra-cluster variance"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        return None
 
+    k, d = C.shape
+    if not isinstance(k, int) or k <= 0:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    M = np.sqrt(((X - C[:, np.newaxis]) ** 2).sum(axis=2))
+    cluster = np.min(M, axis=0)
+    var = np.sum(np.square(cluster))
+
+    return var
