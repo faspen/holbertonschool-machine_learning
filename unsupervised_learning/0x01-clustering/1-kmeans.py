ff0eb94a1ca87b26404d31935cf7d4a787cb0791
faspen <2335@holbertonschool.com>
2021-11-01 16:02:37 +0000

    1-kmeans.py

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -7,4 +7,32 @@ import numpy as np
 
 def kmeans(X, k, iterations=1000):
     """Perform K-means on dataset"""
+    if not isinstance(k, int) or k <= 0:
+        return None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
 
+    n, d = X.shape
+    centroids = np.random.uniform(
+        np.min(X, axis=0), np.max(X, axis=0), size=(k, d))
+
+    for i in range(iterations):
+        tmp = centroids.copy()
+        M = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(M, axis=0)
+
+        for j in range(k):
+            if len(X[clss == j]) == 0:
+                centroids[j] = np.random.uniform(np.min(X, axis=0),
+                                                 np.max(X, axis=0),
+                                                 size=(1, d))
+            else:
+                centroids[j] = (X[clss == j]).mean(axis=0)
+        M = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(M, axis=0)
+        if np.all(tmp == centroids):
+            return centroids, clss
+
+    return centroids, clss
