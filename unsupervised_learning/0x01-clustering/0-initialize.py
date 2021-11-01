e8fbe4772c468c426bca71415b62e8378d3581fb
faspen <2335@holbertonschool.com>
2021-11-01 15:26:05 +0000

    0-initialize.py

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,4 +7,12 @@ import numpy as np
 
 def initialize(X, k):
     """Initialize cluster centroids"""
+    if not isinstance(k, int) or k <= 0:
+        return None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    n, d = X.shape
+    clusters = np.random.uniform(
+        np.min(X, axis=0), np.max(X, axis=0), size=(k, d))
 
+    return clusters
