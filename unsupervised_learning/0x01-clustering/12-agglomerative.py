17f344106c31a1f63b84b09c11f898e3bf988d95
faspen <2335@holbertonschool.com>
2021-11-02 20:43:42 +0000

    12-agglomerative.py

--- unsupervised_learning/0x01-clustering/12-agglomerative.py
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -2,9 +2,18 @@
 """Agglomerative module"""
 
 
-import numpy as np
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
 
 
 def agglomerative(X, dist):
     """Perform agglomerative clustering"""
+    h = scipy.cluster.hierarchy
+    link = h.linkage(X, method='ward')
+    clss = h.fcluster(link, t=dist, criterion='distance')
 
+    plt.figure()
+    h.dendrogram(link, color_threshold=dist)
+    plt.show()
+
+    return clss
