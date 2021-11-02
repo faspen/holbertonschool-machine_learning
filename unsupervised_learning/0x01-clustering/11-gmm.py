fd68b1ca5166b7fee51162d5e78a4a75e741664e
faspen <2335@holbertonschool.com>
2021-11-02 18:29:48 +0000

    bada bing bada boom

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -2,9 +2,17 @@
 """GMM task module"""
 
 
-import numpy as np
+import sklearn.mixture
 
 
 def gmm(X, k):
     """Calculate GMM from a dataset"""
+    GM = sklearn.mixture.GaussianMixture(n_components=k)
+    par = GM.fit(X)
+    pi = par.weights_
+    m = par.means_
+    S = par.covariances_
+    clss = GM.predict(X)
+    bic = GM.bic(X)
 
+    return pi, m, S, clss, bic
