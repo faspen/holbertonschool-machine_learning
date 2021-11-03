df39e5e118f2aac4a0b0453657b871852c8fac67
faspen <2335@holbertonschool.com>
2021-11-03 15:21:17 +0000

    6-grads.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Gradients module"""
+
+
+import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
+
+def grads(Y, P):
+    """Calculate gradients of Y"""
+    return (dY, Q)
