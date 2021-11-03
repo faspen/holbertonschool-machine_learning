fdcf291fff49c7dabac6022075fbbc3a1d40af23
faspen <2335@holbertonschool.com>
2021-11-03 14:56:57 +0000

    7-maximization.py

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -7,4 +7,20 @@ import numpy as np
 
 def maximization(X, g):
     """Calculate maximization step in EM algo for GMM"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or g.ndim != 2:
+        return None, None, None
 
+    n, d = X.shape
+
+    if g.shape[1] != n:
+        return None, None, None
+    k = g.shape[0]
+    if g.shape[0] != k:
+        return None, None, None
+
+    if not np.isclose(np.sum(g, axis=0), np.ones(n,)).all():
+        return None, None, None
+
+    return pi, m, S
