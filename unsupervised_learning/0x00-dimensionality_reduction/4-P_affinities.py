ff80804a2448fbb666d25eb1e8cedb5fb4f24612
faspen <2335@holbertonschool.com>
2021-11-03 15:16:37 +0000

    4-P_affinities.py adv

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""P affinities"""
+
+
+import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """Calculate symmetric P affinities"""
+    return None
