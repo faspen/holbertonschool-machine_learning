c2bab94149e3e1c99cb311700ad98f8a4d21c3d9
faspen <2335@holbertonschool.com>
2021-11-08 16:20:34 +0000

    1-regular.py

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -6,4 +6,25 @@ import numpy as np
 
 
 def regular(P):
+    """Determine steady state probability of chain"""
+    if not isinstance(P, np.ndarray) or P.ndim != 2:
+        return None
+    n, n_s = P.shape
 
+    if n != n_s:
+        return None
+    sums = np.sum(P, axis=1)
+
+    for i in sums:
+        if not np.isclose(i, 1):
+            return None
+
+    evals, enums = np.linalg.eig(P.T)
+    new = enums / enums.sum()
+    new = new.real
+
+    for j in np.dot(new.T, P):
+        if (j >= 0).all() and np.isclose(j.sum(), 1):
+            return j.reshape(1, n)
+
+    return None
