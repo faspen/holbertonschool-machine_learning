74a9d59a66695c1f9f5ae4f1482bce9ce114f450
faspen <2335@holbertonschool.com>
2021-11-16 15:43:36 +0000

    revert changes for now

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -8,11 +8,11 @@ import numpy as np
 class GaussianProcess():
     """Noiseless 1D Gaussian Process class"""
 
-    def __init__(self, X_init, Y_init, lenpar=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """Constructs the class"""
         self.X = X_init
         self.Y = Y_init
-        self.lenpar = lenpar
+        self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(self.X, self.X)
 
@@ -21,4 +21,4 @@ class GaussianProcess():
         shaper = np.sum(X1 ** 2, 1).reshape(-1, 1)
         second = np.sum(X2 ** 2, 1)
         sqdist = shaper + second - 2 * np.dot(X1, X2.T)
-        return self.sigma_f ** 2 * np.exp(-0.5 / self.lenpar ** 2 * sqdist)
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
