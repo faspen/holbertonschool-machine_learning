83414ab91ba31ed3b9233f92da5839dec0e938e8
faspen <2335@holbertonschool.com>
2021-11-16 16:50:12 +0000

    4-bayes_opt.py, add documentation on 3-bayes

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -11,6 +11,7 @@ class BayesianOptimization():
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
                  xsi=0.01, minimize=True):
+        """Class init constructor"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
         min, max = bounds
