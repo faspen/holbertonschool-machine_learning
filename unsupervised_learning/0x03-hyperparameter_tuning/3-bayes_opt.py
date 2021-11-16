7c5bacfa1a1138d757dc4cab7d16d0bdf32215c7
faspen <2335@holbertonschool.com>
2021-11-16 16:32:11 +0000

    3-bayes_opt.py

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -9,3 +9,11 @@ GP = __import__('2-gp').GaussianProcess
 class BayesianOptimization():
     """Perform BO on noiseless 1D Gaussian Process"""
 
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min, max = bounds
+        self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
