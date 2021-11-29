def1c0376ac56f7aad0f89d5db80f7eacad721a4
faspen <2335@holbertonschool.com>
2021-11-29 16:33:34 +0000

    0-rnn_cell.py

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
-""" """
+"""RNN Cell"""
 
 
 import numpy as np
 
 
+class RNNCell():
+    """Class that makes an RNN cell"""
 
+    def __init__(self, i, h, o):
+        """RNN class constructor"""
+        self.Wh = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation"""
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.matmul(concat, self.Wh) + self.bh
+        h_next = np.tanh(h_next)
+        y = np.matmul(h_next, self.Wy) + self.by
+        y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
+        return h_next, y
