0ebacf2f234c8ab7cf5a4b1234ec0f962260be15
faspen <2335@holbertonschool.com>
2021-12-07 16:47:49 +0000

    preprocess_data.py

--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -2,4 +2,15 @@
 """Preprocess Data"""
 
 
+import pandas as pd
 
+
+def preprocess_data(old_data, clean_data):
+    """Preprocess time series data"""
+    cd = pd.read_csv(old_data)
+    cleansed = cd.dropna(axis=0)
+    default = (cleansed - cleansed.mean()) / cleansed.std()
+    default["Timestamp"] = cleansed["Timestamp"]
+    proc = default[["Timestamp", "Volume_(Currency)", "Weighted_Price"]]
+    proc.to_csv(clean_data)
+    proc.head()
