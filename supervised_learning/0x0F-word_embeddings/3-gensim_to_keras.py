4abe313d5944e60eb8a889072ee675bc612b67dc
faspen <2335@holbertonschool.com>
2021-12-13 20:00:46 +0000

    getting mem error on 4

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Extract Word2Vec"""
+
+
+from gensim.models import Word2Vec
 
 
 def gensim_to_keras(model):
-
+    """Convert gensim word2vec model to keras"""
+    return model.wv.get_keras_embedding(train_embeddings=True)
