449e871110b7a12d766c949eb82156beb0333627
faspen <2335@holbertonschool.com>
2021-12-13 19:27:16 +0000

    1-tf_idf.py

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
-""" """
+"""TF-IDF"""
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
+    """Create TF-IDF embedding"""
+    vect = TfidfVectorizer(vocabulary=vocab)
+    mat = vect.fit_transform(sentences)
 
+    emb = mat.toarray()
+    feats = vect.get_feature_names()
+
+    return emb, feats
