977784ddf39a34a1ab452f27702986364eb7cd1a
faspen <2335@holbertonschool.com>
2021-12-30 18:34:35 +0000

    adjustments to call method

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -10,20 +10,19 @@ class SelfAttention(tf.keras.layers.Layer):
 
     def __init__(self, units):
         """Self attention constructor"""
-        super().__init__()
-        self.W = tf.keras.layers.Dense(units)
-        self.U = tf.keras.layers.Dense(units)
-        self.V = tf.keras.layers.Dense(1)
+        super(SelfAttention, self).__init__()
+        self.W = tf.keras.layers.Dense(units=units)
+        self.U = tf.keras.layers.Dense(units=units)
+        self.V = tf.keras.layers.Dense(units=1)
 
     def call(self, s_prev, hidden_states):
         """Perform the embedding"""
-        expanded = tf.expand_dims(input=s_prev, axis=1)
-        inputs = self.U(expanded)
-        hidden = self.W(hidden_states)
+        inputs = self.W(tf.expand_dims(s_prev, 1))
+        hidden = self.U(hidden_states)
         result = self.V(tf.nn.tanh(inputs + hidden))
 
         weights = tf.nn.softmax(result, axis=1)
-        context = weights * expanded
+        context = weights * hidden_states
         context = tf.reduce_sum(context, axis=1)
 
         return context, weights
