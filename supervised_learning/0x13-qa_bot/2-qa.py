bdd29c7261d4867587dbc38f3b9c86cf45137421
faspen <2335@holbertonschool.com>
2022-01-18 16:36:39 +0000

    2-qa.py

---
+++ supervised_learning/0x13-qa_bot/2-qa.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Answer some questions"""
+
+
+import tensorflow as tf
+import tensorflow_hub as hub
+from transformers import BertTokenizer
+
+
+def question_answer(question, reference):
+    """Questions answer method"""
+    tok = BertTokenizer.from_pretrained(
+        "bert-large-uncased-whole-word-masking-finetuned-squad")
+    model = hub.load("https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
+
+    quest_toks = tok.tokenize(question)
+    text_toks = tok.tokenize(reference)
+    tokens = ['[CLS]'] + quest_toks + [
+        '[SEP]'] + text_toks + ['[SEP]']
+
+    input_id = tok.convert_tokens_to_ids(tokens)
+    mask = [1] * len(input_id)
+    input_type = [0] * (1 + len(quest_toks) + 1) + [1] * (len(text_toks) + 1)
+
+    input_id, mask, input_type = map(lambda x: tf.expand_dims(
+        tf.convert_to_tensor(x, tf.int32), 0), (input_id, mask, input_type))
+
+    output = model([input_id, mask, input_type])
+    start = tf.argmax(output[0][0][1:]) + 1
+    end = tf.argmax(output[1][0][1:]) + 1
+    answer_tok = tokens[start: end + 1]
+    answer = tok.convert_tokens_to_string(answer_tok)
+
+    return answer
+
+
+def answer_loop(reference):
+    """Answer question from reference"""
+    while True:
+        question = input("Q: ")
+        question = question.lower().strip()
+        leaving = ["exit", "quit", "goodbye", "bye"]
+
+        if question in leaving:
+            print("A: Goodbye")
+            break
+
+        answer = question_answer(question, reference)
+        if answer is None:
+            print("A: Sorry, I do not understand your question.")
+        else:
+            print("A: ", answer)
