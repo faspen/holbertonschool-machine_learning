a53f8925763c8e67077dee7b9a27c376b5f6404d
faspen <aspensonf@gmail.com>
2022-03-07 11:25:35 -0600

    tasks 0 and 1

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
-""" """
+"""Can I join?"""
 
 
 import requests
 
 
+def availableShips(passengerCount):
+    """Get a list of habitable ships"""
+    if passengerCount < 1:
+        return []
+    ships = []
+    total = []
+    url = 'https://swapi-api.hbtn.io/api/starships/?format=json'
+
+    while 1:
+        r = requests.get(url).json()
+        total += r.get('results')
+        next = r.get('next')
+
+        if next:
+            url = next
+        else:
+            break
+    for ship in total:
+        try:
+            if int(ship.get('passengers').replace(',', '')) >= passengerCount:
+                ships.append(ship.get('name'))
+        except Exception as e:
+            continue
+    return ships
--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
-""" """
+"""Where I am?"""
 
 
 import requests
 
 
+def sentientPlanets():
+    """Return planets with sentient life"""
+    planets = []
+    lifeforms = []
+    url = 'https://swapi-api.hbtn.io/api/species/?format=json'
+
+    while 1:
+        r = requests.get(url).json()
+        lifeforms += r.get('results')
+        next = r.get('next')
+
+        if next:
+            url = next
+        else:
+            break
+    for types in lifeforms:
+        try:
+            if types.get('designation') == 'sentient' or\
+                    types.get('classification') == 'sentient':
+                origin = types.get('homeworld')
+                if origin:
+                    name = requests.get(origin).json().get('name')
+                    planets.append(name)
+        except Exception as e:
+            continue
+    return planets
