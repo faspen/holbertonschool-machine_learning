d5fa65227d36337ab50432b5fced49eb4b73311d
faspen <aspensonf@gmail.com>
2022-03-07 13:09:34 -0600

    2-user_location.py done

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
-""" """
+"""Find Github user"""
 
 
 import requests
+import time
+import sys
 
 
+if __name__ == "__main__":
+    """Print location of Github user"""
+    url = sys.argv[1]
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        if r.status_code == 403:
+            reset = int(r.headers.get('X-Ratelimit-Reset'))
+            current = time.time()
+            min = reset - current
+            min = round(min / 60)
+
+            print("Resest in {} min".format(min))
+            exit()
+    user = r.json()
+    loc = user.get('location')
+
+    if loc:
+        print(user.get('location'))
+    else:
+        print("Not found")
