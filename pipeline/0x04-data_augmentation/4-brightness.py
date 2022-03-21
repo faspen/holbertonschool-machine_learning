53cd3a1a334ef19eb560f98db35fd6061bae65ce
faspen <aspensonf@gmail.com>
2022-03-21 12:54:02 -0500

    tasks 4-5

--- pipeline/0x04-data_augmentation/4-brightness.py
+++ pipeline/0x04-data_augmentation/4-brightness.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Brightness task"""
 
 
 import tensorflow as tf
 
 
+def change_brightness(image, max_delta):
+    """Change the brightness"""
+    return tf.image.adjust_brightness(image, max_delta)
--- pipeline/0x04-data_augmentation/5-hue.py
+++ pipeline/0x04-data_augmentation/5-hue.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Hue task"""
 
 
 import tensorflow as tf
 
 
+def change_hue(image, delta):
+    """Change the hue"""
+    return tf.image.adjust_hue(image, delta)
