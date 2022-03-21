3c903c2778f0214702008b0988a49fea363167c9
faspen <aspensonf@gmail.com>
2022-03-21 12:48:44 -0500

    tasks 2-3, pycodestyle fixes

--- pipeline/0x04-data_augmentation/2-rotate.py
+++ pipeline/0x04-data_augmentation/2-rotate.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Rotate task"""
 
 
 import tensorflow as tf
 
 
+def rotate_image(image):
+    """Rotate an image 90 degrees"""
+    return tf.image.rot90(image)
--- pipeline/0x04-data_augmentation/3-shear.py
+++ pipeline/0x04-data_augmentation/3-shear.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-""" """
+"""Shear task"""
 
 
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 
+def shear_image(image, intensity):
+    """Shear an image"""
+    return tf.compat.v1.keras.preprocessing.image.apply_affine_transform(
+        image.numpy(), shear=intensity)
