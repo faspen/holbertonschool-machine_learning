4506d8c9d1e5b711d6ff20acf3792941113672cf
faspen <aspensonf@gmail.com>
2022-03-21 12:39:16 -0500

    first two tasks

--- pipeline/0x04-data_augmentation/0-flip.py
+++ pipeline/0x04-data_augmentation/0-flip.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Flip task"""
 
 
 import tensorflow as tf
 
 
+def flip_image(image):
+    """Flip the image horizontally"""
+    return tf.image.flip_left_right(image)
--- pipeline/0x04-data_augmentation/1-crop.py
+++ pipeline/0x04-data_augmentation/1-crop.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" """
+"""Crop task"""
 
 
 import tensorflow as tf
 
 
+def crop_image(image, size):
+    """Crop an image"""
+    return tf.image.random_crop(image, size=size)
